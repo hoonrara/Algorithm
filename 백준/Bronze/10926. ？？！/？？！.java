4c5f60cbe1f99cb3d5e80e7afb16e83f6625812b
hoonrara <hoonrara2@gmail.com>
2024-10-18 13:41:33 +0900

    [Bronze V] Title: ??!, Time: 168 ms, Memory: 17696 KB -BaekjoonHub

--- 백준/Bronze/10926. ？？！/？？！.java
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -1,11 +1,10 @@
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String A = sc.next();
+        String id = sc.next();
 
-        System.out.println(A+"??!");
+        System.out.println(id+"??!");
     }
 }
