e7229b0828398e029ad083f0422e89b28de21929
hoonrara <hoonrara2@gmail.com>
2024-10-20 17:19:09 +0900

    Update 오븐 시계.java

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.java
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -11,9 +11,8 @@ public class Main {
         B = B+C;
 
         if (B >59) {
-            A = A+B/60;
+            A = (A+B/60)%24;
             B = B%60;
-            if (A>23) A = A-24;
         }
         System.out.println(A + " " + B);
     }
